52040bfce44da6fc4f38e87de7a2abddb80a97e5
François Lamontagne <flamontagne@gmail.com>
2015-10-03 09:34:23 -0400

    Ajouter des unit test pour les contacts et les réunions

--- spec/models/hive_contact_spec.rb
+++ spec/models/hive_contact_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe HiveContact, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a name" do
+    c = HiveContact.new(full_name: nil)
+    expect(c).to have_exactly(1).error_on(:full_name)
+  end
+
+  it "is invalid without an email" do
+    c = HiveContact.new(email: nil)
+    expect(c).to have_exactly(1).error_on(:email)
+  end
+
+  it "is invalid when an invalid email is supplied" do
+    c = HiveContact.new(email: "Notanemail")
+    expect(c).to have_exactly(1).error_on(:email)
+  end
+
+  it "is valid when all fields are entered correctly" do
+    c = HiveContact.new(full_name: "Hello", email: "iam@valid.com")
+    expect(c).to be_valid
+  end
+
 end
--- spec/models/hive_meeting_spec.rb
+++ spec/models/hive_meeting_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe HiveMeeting, :type => :model do
+  fixtures :hives, :hive_meetings
   it "is invalid without a location" do
     meeting = HiveMeeting.new(location: nil)
     expect(meeting).to have_exactly(1).error_on(:location)
@@ -19,5 +20,32 @@ RSpec.describe HiveMeeting, :type => :model do
   it "is invalid without a meeting time" do
     meeting = HiveMeeting.new(meeting_time: nil)
     expect(meeting).to have_exactly(1).error_on(:meeting_time)
+  end
+
+  it "must have a valid meeting date" do
+    meeting = HiveMeeting.new(meeting_date: "I'm not a date")
+    expect(meeting).to have_exactly(1).error_on(:meeting_date)
+  end
+
+  it "must have a valid meeting time" do
+    meeting = HiveMeeting.new(meeting_time: "Not a time at all")
+    expect(meeting).to have_exactly(1).error_on(:meeting_time)
+  end
+
+  it "is valid when all fields are entered correctly" do
+    meeting = HiveMeeting.new(location: "Restaurant XYZ", address: "1234 bonjour", meeting_date: Date.today, meeting_time: Time.now)
+    expect(meeting).to be_valid
+  end
+
+  context "Class methods" do
+    it "has a class method to return past meetings" do
+      meetings = hives(:vive_le_quebec).hive_meetings.upcoming
+      expect(meetings.count).to eq 1
+    end
+
+    it "has a class method to return upcoming meetings" do
+      meetings = hives(:vive_le_quebec).hive_meetings.past
+      expect(meetings.count).to eq 1
+    end
   end
 end
