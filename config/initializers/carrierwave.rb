7dc8c92034947ec54e57e6c50cfdb1b91b18339f
François Lamontagne <flamontagne@gmail.com>
2015-10-03 10:01:24 -0400

    Ajouter options de configuration pour accès à AWS S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,9 @@ CarrierWave.configure do |config|
     config.storage = :fog    
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :region                 => 'us-west-2'
+      :region                 => 'us-west-2',
+      :aws_access_key_id      => ENV['aws_access_key_id'],
+      :aws_secret_access_key  => ENV['aws_secret_access_key']
     }
     config.fog_directory  = 'accq'                          # required
     config.fog_public     = true                                        # optional, defaults to true
