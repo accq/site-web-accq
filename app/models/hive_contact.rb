f9af5bda1b18845dc77bfe1773a6a4bfcc016b3f
François Lamontagne <flamontagne@gmail.com>
2015-10-05 11:38:54 -0400

    Le courriel d'un responsable d'ACRQ n'est plus obligatoire

--- app/models/hive_contact.rb
+++ app/models/hive_contact.rb
@@ -1,5 +1,5 @@
 class HiveContact < ActiveRecord::Base
   belongs_to :hive
   validates :full_name, presence: true
-  validates :email, email: true  
+  validates :email, email: true, allow_blank: true
 end
--- spec/models/hive_contact_spec.rb
+++ spec/models/hive_contact_spec.rb
@@ -6,18 +6,13 @@ RSpec.describe HiveContact, :type => :model do
     expect(c).to have_exactly(1).error_on(:full_name)
   end
 
-  it "is invalid without an email" do
-    c = HiveContact.new(email: nil)
-    expect(c).to have_exactly(1).error_on(:email)
-  end
-
   it "is invalid when an invalid email is supplied" do
     c = HiveContact.new(email: "Notanemail")
     expect(c).to have_exactly(1).error_on(:email)
   end
 
   it "is valid when all fields are entered correctly" do
-    c = HiveContact.new(full_name: "Hello", email: "iam@valid.com")
+    c = HiveContact.new(full_name: "Hello")
     expect(c).to be_valid
   end
 
