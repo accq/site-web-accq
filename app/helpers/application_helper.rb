97919891efda0665022ba794f16926b14539dc2d
François Lamontagne <flamontagne@gmail.com>
2015-11-27 13:34:09 -0500

    Meilleur affichage du range de date pour les événements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def show_hive_number(hive_number)
     sprintf '%02d', hive_number
   end
+
+  def show_date_range(dt_start,dt_end)
+    if dt_start == dt_end
+      "Le #{l dt_start, format: :long}"
+    elsif dt_start.month == dt_end.month
+      "Les #{dt_start.day} et #{dt_end.day} #{l dt_end, format: :just_month} prochains"
+    else
+      "Les #{l dt_start, format: :no_year} et #{l dt_end, format: :no_year} prochains"
+    end
+  end
 end
