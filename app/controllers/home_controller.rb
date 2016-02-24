fa2500feccbc511e3a74e233a10e8812b4fe557b
Francois-Marchildon <vous.ecrivez@francoismarchildon.com>
2016-02-24 16:35:32 -0500

    FM: Fix pour forcer le download du PDF questionnaire-bilan, au lieu de l'ouvrir dans le browser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,12 @@ class HomeController < ApplicationController
     @hives = Hive.order(:number)
     @next_event = Event.upcoming.first
   end
+
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/Questionnaire-bilan-février-ACCQ.pdf",
+      :filename => "Questionnaire-bilan-février-ACCQ.pdf",
+      :type => "application/pdf"
+    )
+  end
 end
