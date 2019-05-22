22b5494bcf823b0dc8de5a11ad3034dbc9cec2a9
François Lamontagne <deneb@deneb999>
2019-05-21 22:00:06 -0400

    fix script import_members

--- lib/import_members.rb
+++ lib/import_members.rb
@@ -1,7 +1,12 @@
 require 'csv'
 
-members = []
+#members = []
 CSV.foreach(Rails.root.to_s + "/lib/signatures_a_jour.csv", col_sep: ",", headers: true) do |row|
-  members << Member.new(status: row[0], name: row[2].to_s + ' ' + row[1].to_s, email: row[3].to_s, phone: row[4].to_s, city: row[5].to_s, postal_code: row[6].to_s.delete(' ').upcase, confirmed_at: row[8] ? row[8].to_s.to_date : 2.years.ago)
+  existing_member = Member.where("email = ?", row[3].to_s.strip).first
+  if existing_member
+    existing_member.update_attributes(status: row[0], name: row[2].to_s + ' ' + row[1].to_s,  phone: row[4].to_s, city: row[5].to_s, postal_code: row[6].to_s.delete(' ').upcase, confirmed_at: row[8] ? row[8].to_s.to_date : 2.years.ago)
+  else
+    Member.create(status: row[0], name: row[2].to_s + ' ' + row[1].to_s, email: row[3].to_s.strip, phone: row[4].to_s, city: row[5].to_s, postal_code: row[6].to_s.delete(' ').upcase, confirmed_at: row[8] ? row[8].to_s.to_date : 2.years.ago)
+  end
 end
-Member.import(members, on_duplicate_key_ignore: true)
+#Member.import(members, on_duplicate_key_ignore: true)
